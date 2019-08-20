1b69345ea1a3595340a6edd717bb10b1d2027636
Hariom Pandey <hariom.pandey@lapr144.successivetech.com>
2019-08-20 18:32:35 +0530

    set up apollo task

--- dist/ config /configuration.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-dotenv_1.config();
-const configuration = Object.freeze({
-    port: process.env.PORT,
-});
-exports.default = configuration;
-//# sourceMappingURL=configuration.js.map
