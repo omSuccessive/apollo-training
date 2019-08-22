c0b0b11338984cd189e0bc055f66d00c05b7decf
Hariom Pandey <hariom.pandey@lapr144.successivetech.com>
2019-08-22 17:02:56 +0530

    Adding Query Resolvers and Services call task

--- src/server.js
+++ src/server.js
@@ -13,9 +13,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-// app.listen({ port: configuration.port }, () => {
-//   console.log('server is ready');
-// });
+
 app.listen({ port: 4000 }, () =>
             console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
         );
